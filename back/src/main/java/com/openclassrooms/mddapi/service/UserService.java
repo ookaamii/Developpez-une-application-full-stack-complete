d32f685b39cd5eeaca709c8d5cc45b46b03ee4b8
jess <jess@jess-okami>
2024-12-15 23:22:07 +0100

    modification composants + mise en place fonctions du profil

--- back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/UserService.java
@@ -1,8 +1,11 @@
 package com.openclassrooms.mddapi.service;
 
+import com.openclassrooms.mddapi.dto.UserDTO;
+import com.openclassrooms.mddapi.dto.UserUpdateDTO;
 import com.openclassrooms.mddapi.dto.request.LoginDTO;
 import com.openclassrooms.mddapi.dto.request.RegisterDTO;
 import com.openclassrooms.mddapi.dto.response.AuthDTO;
+import com.openclassrooms.mddapi.dto.response.ResponseDTO;
 import com.openclassrooms.mddapi.model.User;
 
 public interface UserService {
@@ -12,5 +15,9 @@ public interface UserService {
     AuthDTO login(LoginDTO loginDTO);
 
     User getAuthenticatedUser();
+
+    UserUpdateDTO getProfile();
+
+    ResponseDTO update(UserUpdateDTO userDTO);
 
 }
