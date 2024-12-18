927caf086a245ed3f172dd274af6dfe80bfd8f82
jess <jess@jess-okami>
2024-12-18 18:19:18 +0100

    rajout methode findAllByTopic pour post

--- back/src/main/java/com/openclassrooms/mddapi/service/PostService.java
+++ back/src/main/java/com/openclassrooms/mddapi/service/PostService.java
@@ -4,10 +4,14 @@ import com.openclassrooms.mddapi.dto.request.PostRequestDTO;
 import com.openclassrooms.mddapi.dto.response.PostResponseDTO;
 import com.openclassrooms.mddapi.dto.response.ResponseDTO;
 
+import java.util.List;
+
 public interface PostService {
 
     PostResponseDTO getPost(Long id);
 
     ResponseDTO create(PostRequestDTO postDTO);
+
+    List<PostResponseDTO> findAllByTopics();
 
 }
